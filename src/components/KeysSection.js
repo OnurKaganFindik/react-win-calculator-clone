8534017e2aa63a67d56dc28cef53fb9a9944258f
OnurKaganFindik <45922689+OnurKaganFindik@users.noreply.github.com>
2021-11-03 17:18:23 +0300

    KeyButton functionaty

--- src/components/KeysSection.js
+++ src/components/KeysSection.js
@@ -1,16 +1,48 @@
+import KeyButton from "./KeyButton";
+
 const styles = {
     container: {
         width: '100%',
         height: '66%',
         display: 'flex',
+        flexWrap: 'wrap',
     },
 };
 
 const KeysSection = () => {
     return (
         <div style={styles.container}>
-        </div>
-    )
+      <KeyButton label="%" />
+      <KeyButton label="CE" />
+      <KeyButton label="C" />
+      <KeyButton label="<-" />
+
+      <KeyButton label="1/x" />
+      <KeyButton label="x^2" />
+      <KeyButton label="kök(x)" />
+      <KeyButton label="/" />
+
+      <KeyButton label="7" isNumber />
+      <KeyButton label="8" isNumber />
+      <KeyButton label="9" isNumber />
+      <KeyButton label="X" />
+
+      <KeyButton label="4" isNumber />
+      <KeyButton label="5" isNumber />
+      <KeyButton label="6" isNumber />
+      <KeyButton label="-" />
+
+      <KeyButton label="1" isNumber />
+      <KeyButton label="2" isNumber />
+      <KeyButton label="3" isNumber />
+      <KeyButton label="+" />
+
+      <KeyButton label="+/-" isNumber />
+      <KeyButton label="0" isNumber />
+      <KeyButton label="," isNumber />
+      <KeyButton label="=" isBlue />
+    </div>
+  );
 };
 
 
--- src/components/ScreenSection.js
+++ src/components/ScreenSection.js
@@ -10,12 +10,18 @@ const styles = {
         fontSize: 30,
         padding: 10,
     },
+    caption: {
+        color: '#999',
+        fontSize: 17,
+        paddingRight: 0,
+    },
 };
 
 
 const ScreenSection = () => {
     return (
         <div style={styles.screenSection}>
+            <span style={styles.caption}> 25</span>
             <span style={styles.mainText}>
                 asd
             </span>
